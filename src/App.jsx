10f43df5b66829fd2021da38ac0083b7ab074031
habibur62 <habibur.vendabari@gmail.com>
2024-09-12 16:11:08 +0600

    calculation complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,68 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setweight] = useState(0);
+  const [height, setheight] = useState(0);
+  const [bmi, setBmi] = useState(0);
+  const  [message, setmessage] = useState("");
+
+  const weighthandler = (e) =>{
+    e.preventDefault();
+    setweight(e.target.value);
+  }
+
+  useEffect(()=>{
+
+  },[])
+
+  const heighthandler = (e) =>{
+    e.preventDefault();
+    setheight(e.target.value);
+  }
+
+  useEffect(()=>{
+  },[])
+
+
+  const countBmi =(e)=>{
+    e.preventDefault();
+    let cal = .0254*height;
+   cal = (weight/(cal*cal)) ;
+   setBmi(cal.toFixed(1));  
+
+   if(cal<=18.4){
+    setmessage("Underwright")
+   }else if(cal >=18.5 && cal<=24.9){
+    setmessage("Normal")
+
+   }else if(25<=cal && cal<= 39.9){
+    setmessage("Overweight")
+
+   }
+   else{
+    setmessage("Obese")
+
+   }
+  }
 
   return (
     <>
-        <h2>hello
+      <div className="container">
+        <div className="app-body">
+          <h2>BMI APP</h2>
+          <form className='form-body' onSubmit={countBmi}>
+            <input onChange={weighthandler} type="number"  name='weight' className='weight'placeholder='your weight in kg unit...' required/>
+            <input  onChange={heighthandler} type="number" name='height' className='height' placeholder='your height in meters...' required/>
+            <button type='submit'>Submit</button>
           
-        </h2>
-
+          </form>
+          <p>BMI = {bmi}</p>
+          <p>Message:{message}</p>
+        </div>
+      </div>
     </>
   )
 }
